1b74235a32962bf93605d3e991224a94c533b5a0
shi-314 <shi.taher@gmail.com>
2015-11-26 22:48:38 +0100

    Add caffe deps

--- .vimrc
+++ .vimrc
@@ -1,3 +1,4 @@
+set shell=/bin/bash
 set nocompatible              " be iMproved, required
 filetype off                  " required
 
--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -7,3 +7,9 @@ chsh -s `which fish`
 
 sudo apt-get install compizconfig-settings-manager
 sudo apt-get install mesa-utils
+
+# caffe
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+sudo apt-get install libatlas-base-dev
