6fc4b0f28e8d47e782ca2e2dbcbde96f70a873b8
Shivan <shi.taher@gmail.com>
2015-11-26 23:34:18 +0100

    Add cuda paths

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -17,3 +17,4 @@ sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev 
 sudo apt-get install --no-install-recommends libboost-all-dev
 sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
 sudo apt-get install libatlas-base-dev
+sudo apt-get install protobuf glog gflags hdf5
