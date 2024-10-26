0c1506944f13b36402c282e1c29d01698f5bb1f9
Shivan Taher <shi.taher@gmail.com>
2024-10-26 15:21:05 +0200

    Update

---
+++ .wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = 'hardhacker'
+config.window_background_opacity = 0.9
+config.hide_tab_bar_if_only_one_tab = true
+config.font_size = 11
+
+
+return config
